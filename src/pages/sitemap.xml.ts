56afea5f08723f380e8e1e0357c9f59379121ef0
第九 <2331613886@qq.com>
2026-09-04 21:28:54 +0800

    refactor: streamline page layout, update footer branding and mascot tap styling

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,27 +2,16 @@ import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
 export const GET: APIRoute = async ({ site }) => {
-  const [articles, activities, projects, theater] = await Promise.all([
-    getCollection('articles', ({ data }) => !data.draft),
-    getCollection('activities', ({ data }) => !data.draft),
-    getCollection('projects', ({ data }) => !data.draft),
-    getCollection('theater', ({ data }) => !data.draft),
-  ]);
+  const articles = await getCollection('articles', ({ data }) => !data.draft);
   const paths = [
     '/',
     '/about',
     '/articles',
     '/articles/news',
     '/articles/blog',
-    '/activities',
-    '/projects',
-    '/theater',
     '/join',
   ];
   paths.push(...articles.map((item) => '/articles/' + item.id));
-  paths.push(...activities.map((item) => '/activities/' + item.id));
-  paths.push(...projects.map((item) => '/projects/' + item.id));
-  paths.push(...theater.map((item) => '/theater/' + item.id));
   const base = site?.toString().replace(/\/$/, '') || 'https://cxcs.dev';
   const body = paths.map((path) => '<url><loc>' + base + path + '</loc></url>').join('');
   return new Response(
